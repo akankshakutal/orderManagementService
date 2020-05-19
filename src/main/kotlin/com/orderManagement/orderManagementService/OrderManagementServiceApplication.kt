6d19ad32a8db525796419cc23217aa4d74cc436d
akankshakutal <akankshk@thoughtworks.com>
2020-05-19 19:11:18 +0530

    Initial commit

---
+++ src/main/kotlin/com/orderManagement/orderManagementService/OrderManagementServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.orderManagement.orderManagementService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrderManagementServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrderManagementServiceApplication>(*args)
+}
